318232447387366210573edd41d7c72ef27c026b
hatanvir <tanvirhasan552@gmail.com>
2018-09-09 23:07:47 +0600

    Weather api updated

--- app/src/main/java/com/example/tanvir/weatherreport/WeatherApi.java
+++ app/src/main/java/com/example/tanvir/weatherreport/WeatherApi.java
@@ -1,7 +1,7 @@
 package com.example.tanvir.weatherreport;
 
 import com.example.tanvir.weatherreport.models.forecast_models.Forecast;
-import com.example.tanvir.weatherreport.models.forecast_models.Weather;
+import com.example.tanvir.weatherreport.models.weather_models.Weather1;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -10,15 +10,15 @@ import retrofit2.http.Url;
 public interface WeatherApi {
 
     @GET("data/2.5/weather?q=Dhaka,bd&apikey=90ff8755cfe4bfaa6e542e82cafe5b3e")
-    Call<com.example.tanvir.weatherreport.models.weather_models.Weather> getWeather();
+    Call<Weather1> getWeather();
 
     @GET()
-    Call<com.example.tanvir.weatherreport.models.weather_models.Weather>getWeatherBySearch(@Url String location);
+    Call<Weather1>getWeatherBySearch(@Url String location);
 
 
     @GET("data/2.5/forecast/daily?q=Dhaka,bd&units=metric&cnt=7&appid=c0c4a4b4047b97ebc5948ac9c48c0559")
     Call<Forecast>getForecast();
 
-    @GET
+    @GET()
     Call<Forecast>getForecastBySearch(@Url String foreCast);
 }
