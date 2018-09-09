91fe5cca4d22be460c45997df3585d4255b03bff
hatanvir <tanvirhasan552@gmail.com>
2018-09-09 23:12:11 +0600

    Current weather fragment development complete

--- app/src/main/java/com/example/tanvir/weatherreport/fragments/CurrectWeatherFragment.java
+++ app/src/main/java/com/example/tanvir/weatherreport/fragments/CurrectWeatherFragment.java
@@ -2,29 +2,23 @@ package com.example.tanvir.weatherreport.fragments;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.tanvir.weatherreport.Activity.MainActivity;
 import com.example.tanvir.weatherreport.R;
 import com.example.tanvir.weatherreport.RetrofitClient;
 import com.example.tanvir.weatherreport.WeatherApi;
-import com.example.tanvir.weatherreport.models.weather_models.Weather;
+import com.example.tanvir.weatherreport.models.weather_models.Weather1;
+import com.squareup.picasso.Picasso;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import retrofit2.Call;
@@ -34,10 +28,13 @@ import retrofit2.Response;
 public class CurrectWeatherFragment extends Fragment {
 
     WeatherApi weatherApi;
+    SharedPreferences myPrefs;
 
+    TextView tempText, dateText, dayText,
+            cityText, conditionText, minText,
+            maxText, sunriseText, sunsetText,
+            humidityText, pressureText;
 
-    TextView tempText, dateText, dayText, cityText, conditionText, minText, maxText,
-            sunriseText, sunsetText, humidityText, pressureText;
     ImageView conditionImage;
 
 
@@ -74,73 +71,139 @@ public class CurrectWeatherFragment extends Fragment {
         humidityText = view.findViewById(R.id.humidityTextview);
         pressureText = view.findViewById(R.id.pressureTextview);
 
+        myPrefs = getActivity().getSharedPreferences("prefID", Context.MODE_PRIVATE);
+        String key = myPrefs.getString("Key",null);
+        getWeatherData(key);
+
+        return  view;
+    }
+//getting default weather data
+    private void getWeatherData(final String key) {
         weatherApi = RetrofitClient.getRetrofitClient().create(WeatherApi.class);
 
-        final Call<Weather> getWetherCall = weatherApi.getWeather();
+        final Call<Weather1> getWetherCall = weatherApi.getWeather();
 
-        getWetherCall.enqueue(new Callback<Weather>() {
+        getWetherCall.enqueue(new Callback<Weather1>() {
             @Override
-            public void onResponse(Call<Weather> call, Response<Weather> response) {
-                Weather weather = response.body();
+            public void onResponse(Call<Weather1> call, Response<Weather1> response) {
+                Weather1 weather = response.body();
 
+                String iconUrl = "http://openweathermap.org/img/w/" + weather.getWeather().get(0).getIcon()+ ".png";
                 double temparature = (double)(weather.getMain().getTemp() - 273.15);
-                tempText.setText(Double.toString(temparature)+ " " + "\u2103");
                 double min = (double)(weather.getMain().getTempMin() - 273.15);
-                minText.setText(Double.toString(min)+ " " + "\u2103");
                 double max = (double)(weather.getMain().getTempMax() - 273.15);
-                maxText.setText(Double.toString(max)+ " " + "\u2103");
 
+                if(key.equals("1") || key==null){//setting celsius
+                    tempText.setText(new DecimalFormat("#.#").format(temparature) + " " + "\u2103");
+                    minText.setText(new DecimalFormat("#.#").format(min) + " " + "\u2103");
+                    maxText.setText(new DecimalFormat("#.#").format(max) + " " + "\u2103");
+                }
+                else if(key.equals("2")){//setting farenhite
+                    tempText.setText(kelvinToFarenhite(weather.getMain().getTemp()));
+                    minText.setText(kelvinToFarenhite(weather.getMain().getTempMin()));
+                    maxText.setText(kelvinToFarenhite(weather.getMain().getTempMax()));
+                }
 
                 dateText.setText(dateFormate(weather.getDt()));
                 dayText.setText(dayFormate(weather.getDt()));
                 cityText.setText(weather.getName());
+                sunriseText.setText(timeFormate(weather.getSys().getSunrise()));
+                sunsetText.setText(timeFormate(weather.getSys().getSunset()));
+                humidityText.setText(weather.getMain().getHumidity().toString()+" %");
+                pressureText.setText(weather.getMain().getPressure().toString()+" hPa");
+                conditionText.setText(weather.getWeather().get(0).getDescription());
 
-                int sunrise = weather.getSys().getSunrise();
-                sunriseText.setText(timeFormate(sunrise)+ " AM");
-                int sunset = weather.getSys().getSunset();
-                sunsetText.setText(timeFormate(sunset) + " PM");
-
+                Picasso.with(getActivity()).load(iconUrl).into(conditionImage);//call picasso for image loading
             }
 
             @Override
-            public void onFailure(Call<Weather> call, Throwable t) {
+            public void onFailure(Call<Weather1> call, Throwable t) {
 
             }
         });
+    }
+//getting data by search
+    public void getWeatherDataBySearch(String location, final String key) {
 
-        return  view;
+        Toast.makeText(getActivity(), "checked", Toast.LENGTH_SHORT).show();
+        weatherApi = RetrofitClient.getRetrofitClient().create(WeatherApi.class);
+
+        String url = "data/2.5/weather?q="+location+"&apikey=90ff8755cfe4bfaa6e542e82cafe5b3e";
+
+        Call<Weather1> weather1Call = weatherApi.getWeatherBySearch(url);
+        weather1Call.enqueue(new Callback<Weather1>() {
+            @Override
+            public void onResponse(Call<Weather1> call, Response<Weather1> response) {
+                Toast.makeText(getActivity(), "checked reponse", Toast.LENGTH_SHORT).show();
+                if(response.isSuccessful() && response.body()!=null){
+
+                    Weather1 weather2 = response.body();
+
+                    String iconUrl = "http://openweathermap.org/img/w/" + weather2.getWeather().get(0).getIcon()+ ".png";
+                    double temparature = (double)(weather2.getMain().getTemp() - 273.15);
+                    double min = (double)(weather2.getMain().getTempMin() - 273.15);
+                    double max = (double)(weather2.getMain().getTempMax() - 273.15);
+
+                    if(key.equals("1") || key==null){
+                        tempText.setText(new DecimalFormat("#.#").format(temparature) + " " + "\u2103");
+                        minText.setText(new DecimalFormat("#.#").format(min) + " " + "\u2103");
+                        maxText.setText(new DecimalFormat("#.#").format(max) + " " + "\u2103");
+                    }
+                    else if(key.equals("2")){
+                        tempText.setText(kelvinToFarenhite(weather2.getMain().getTemp()));
+                        minText.setText(kelvinToFarenhite(weather2.getMain().getTempMin()));
+                        maxText.setText(kelvinToFarenhite(weather2.getMain().getTempMax()));
+                    }
+
+                    dateText.setText(dateFormate(weather2.getDt()));
+                    dayText.setText(dayFormate(weather2.getDt()));
+                    cityText.setText(weather2.getName());
+                    sunriseText.setText(timeFormate(weather2.getSys().getSunrise()));
+                    sunsetText.setText(timeFormate(weather2.getSys().getSunset()));
+                    humidityText.setText(weather2.getMain().getHumidity().toString()+"%");
+                    pressureText.setText(weather2.getMain().getPressure().toString()+" hPa");
+                    conditionText.setText(weather2.getWeather().get(0).getDescription());
+
+                    Picasso.with(getActivity()).load(iconUrl).into(conditionImage);//call picasso for image loading
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Weather1> call, Throwable t) {
+
+            }
+        });
     }
 
-    public void OnQueryTextListener(String text) {
-        Log.d("Tanvir",""+text);
-    }
 
-
-    // date formate
+    // date format from unix value
     private String dateFormate(int dt) {
         Date date = new java.util.Date(dt*1000L);
-        SimpleDateFormat sdf = new java.text.SimpleDateFormat("MMMM dd, yyyy");
-        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT-4"));
+        SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+6"));
         String formattedDate = sdf.format(date);
         return formattedDate;
     }
-    // get current day
+    // day format from unix value
     private String dayFormate(int dt) {
         Date date = new java.util.Date(dt*1000L);
         SimpleDateFormat sdf = new java.text.SimpleDateFormat("EEEE");
-        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT-4"));
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+6"));
+        String formattedDate = sdf.format(date);
+        return formattedDate;
+    }
+//time format from unix value
+    private String timeFormate(int dt) {
+        Date date = new java.util.Date(dt*1000L);
+        SimpleDateFormat sdf = new java.text.SimpleDateFormat("hh:mm:ss a");
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+6"));
         String formattedDate = sdf.format(date);
         return formattedDate;
     }
 
-    // time formate
-    private String timeFormate(int dt) {
-        Date date = new java.util.Date(dt*1000L);
-        SimpleDateFormat sdf = new java.text.SimpleDateFormat("HH:mm");
-        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT-4"));
-        String formattedDate = sdf.format(dt);
-
-        return formattedDate;
+    public String kelvinToFarenhite(double k){
+       double f = (9/5*(k - 273) + 32);
+        String farenhite = new DecimalFormat("#.#").format(f) + " " + "\u2109";
+        return  farenhite;
     }
-
 }
